3ef1baf227497a2cd19b9c3d05f05276a252a197
Felipe <felipe.balcero45@gmail.com>
2018-11-20 22:20:16 -0500

    Schema created

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObejctType, GraphQLString } = graphql;
+const { GraphQLObejctType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObejctType({
     name: 'Book',
@@ -9,4 +9,21 @@ const BookType = new GraphQLObejctType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     }
+});
+
+const RootQuery = new GraphQLObejctType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: {type: GraphQLString}},
+            resolve(parent, args) {
+                // Code to get results
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
